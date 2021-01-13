7130044a607366f8711d78a2a12332b609b217ae
Joshua Galit <joshuaimalay@gmail.com>
2021-01-13 09:54:10 +0800

    add basic layout menu submenu and content using flex

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,21 @@
 <template>
-  <div>
-    
+  <div class="flex flex-row h-screen">
+    <div class="w-16 bg-gray-200 flex flex-col justify-between items-center p-4">
+      <div class="flex flex-col space-y-3">
+        <a href="#"><div class="rounded-full bg-gray-400 w-8 h-8"></div></a>
+        <a href="#"><div class="rounded-full bg-gray-400 w-8 h-8"></div></a>
+        <a href="#"><div class="rounded-full bg-gray-400 w-8 h-8"></div></a>
+        <a href="#"><div class="rounded-full bg-gray-400 w-8 h-8"></div></a>
+        <a href="#"><div class="rounded-full bg-gray-400 w-8 h-8"></div></a>
+      </div>
+      <div class="flex flex-col space-y-3">
+        <a href="#"><div class="rounded-full bg-gray-400 w-8 h-8"></div></a>
+        <a href="#"><div class="rounded-full bg-gray-400 w-8 h-8"></div></a>
+        <a href="#"><div class="rounded-full bg-gray-400 w-8 h-8"></div></a>
+      </div>
+    </div>
+    <div class="w-64">submenu</div>
+    <div class="flex-grow bg-yellow-200">content</div>
   </div>
 </template>
 
